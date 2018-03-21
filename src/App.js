d29cf4d8d97825a1958b49831a9b7c0ab8d444c3
Konekoya <tank158r5155@yahoo.com.tw>
2018-03-22 06:40:07 +0800

    Basic UIs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="container">
+        <header>
+          <h1 className="branding">ParseLtongue</h1>
+          <span className="slogan">Anything is parsable</span>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <form action="">
+            <input className="input" type="text" />
+            <button>Parse</button>
+          </form>
+        </main>
       </div>
     );
   }
